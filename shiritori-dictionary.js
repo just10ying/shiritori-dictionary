d94594a435668c8a1cedb5db17245e0d590a0dd2
Justin Ying <justinying@justinying0.mtv.corp.google.com>
2016-09-19 14:17:56 -0700

    Fixed regex

--- shiritori-dictionary.js
+++ shiritori-dictionary.js
@@ -6,24 +6,19 @@ const MASHAPE_API_KEY = 'Hf6g30e7hsmsh7Z90Ty8JGj4QqxRp1Lvbcpjsn99NlC069SThT';
 
 function getQueryString(letter) {
   var requestObject = {
-    letterPattern: '^' + letter + '[a-z]+',
+    letterPattern: '^' + letter + '[a-z]+$',
     random: true
   };
   return '?' + querystring.stringify(requestObject);
 }
 
 module.exports = {
-  getWordBeginningWith: function(letter, usedWordsArray, callback) {
+  getWordBeginningWith: function(letter, callback) {
     unirest.get(DICTIONARY_API_URL + getQueryString(letter))
     .header("X-Mashape-Key", MASHAPE_API_KEY)
     .header("Accept", "application/json")
     .end(function(result) {
-      var word = result.body.word;
-      if (usedWordsArray.indexOf(word) < 0) {
-        callback(word);
-        return;
-      }
-      callback(null);
+      callback(result.body.word);
     });
   },
   validateWord: function(word, callback) {
