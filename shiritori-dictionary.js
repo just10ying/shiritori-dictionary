90af6a29e630b3f05abb25f4705a2e2873a363cd
Justin Ying <justinying@justinying0.mtv.corp.google.com>
2016-09-19 11:02:43 -0700

    Added word validation function and some bad tests.

--- shiritori-dictionary.js
+++ shiritori-dictionary.js
@@ -1,7 +1,7 @@
 const querystring = require('querystring');
 const unirest = require('unirest');
 
-const DICTIONARY_API_URL = 'https://wordsapiv1.p.mashape.com/words/?';
+const DICTIONARY_API_URL = 'https://wordsapiv1.p.mashape.com/words/';
 const MASHAPE_API_KEY = 'Hf6g30e7hsmsh7Z90Ty8JGj4QqxRp1Lvbcpjsn99NlC069SThT';
 
 function getQueryString(letter) {
@@ -9,7 +9,7 @@ function getQueryString(letter) {
     letterPattern: '^' + letter,
     random: true
   };
-  return querystring.stringify(requestObject);
+  return '?' + querystring.stringify(requestObject);
 }
 
 module.exports = {
@@ -24,6 +24,15 @@ module.exports = {
         return;
       }
       callback(null);
+    });
+  },
+  validateWord: function(word, callback) {
+    unirest.get(DICTIONARY_API_URL + word)
+    .header("X-Mashape-Key", MASHAPE_API_KEY)
+    .header("Accept", "application/json")
+    .end(function(result) {
+      if (result.code != 200) callback(false);
+      else callback(true);
     });
   }
 };
